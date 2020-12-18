712210a79a039d0d6cc23918567ef4ba2459a03c
Carlos alves <carlosmspa@netcabo.pt>
2020-12-18 21:16:29 +0000

    Criar entidade animal e o dataContext

---
+++ MVC_VET.Web/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_VET.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_VET.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+
+        // obter da DB os Animais
+        public DbSet<Animal> Animals { get; set; }
+
+    }
+}
---
+++ MVC_VET.Web/Data/Entities/Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_VET.Web.Data.Entities
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+
+
+        public string Nome { get; set; }
+
+        [Display(Name = "Foto")]
+        public string ImageUrl { get; set; }
+
+
+        [Display(Name = "Data de Nascimento")]
+        public DateTime? DataNascimento { get; set; }
+
+        [Display(Name = "Espécie")]
+        public string Especie { get; set; }
+
+        [Display(Name = "Raça")]
+        public string Raca { get; set; }
+
+
+    }
+}

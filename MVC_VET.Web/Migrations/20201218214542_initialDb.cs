06c2b025f9144706431d6e0586251cfa0f87066b
Carlos alves <carlosmspa@netcabo.pt>
2020-12-21 19:55:38 +0000

    Create DB table animal

---
+++ MVC_VET.Web/Migrations/20201218214542_initialDb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVC_VET.Web.Migrations
+{
+    public partial class initialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    DataNascimento = table.Column<DateTime>(nullable: true),
+                    Especie = table.Column<string>(nullable: true),
+                    Raca = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
